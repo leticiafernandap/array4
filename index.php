756cbcc69a39a8f5e704894f3f6fcf20a224b065
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-03-08 11:53:22 -0300

    array

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+    $pessoa1 = array();
+    $pessoa2 = array();
+    $pessoa3 = array();
+    $pessoa4 = array();
+
+    $pessoa1[0][0][0][0][0]["alunos"] = "Maria";
+    $pessoa1[0][0][0][0][0]["idade"] = "16 anos";
+    $pessoa1[0][0][0][0][0]["cidade"] = "Ivaiporã";
+    $pessoa1[0][0][0][0][0]["característica"] = "inteligente";
+
+    $pessoa2[0][0][0][0][0]["alunos"] = "Ana";
+    $pessoa2[0][0][0][0][0]["idade"] = "16 anos";
+    $pessoa2[0][0][0][0][0]["cidade"] = "Ivaiporã";
+    $pessoa2[0][0][0][0][0]["característica"] = "dedicada";
+
+    $pessoa3[0][0][0][0][0]["alunos"] = "Matheus";
+    $pessoa3[0][0][0][0][0]["idade"] = "16 anos";
+    $pessoa3[0][0][0][0][0]["cidade"] = "Jardim Alegre";
+    $pessoa3[0][0][0][0][0]["característica"] = "esforçado";
+
+    $pessoa4[0][0][0][0][0]["alunos"] = "Letícia";
+    $pessoa4[0][0][0][0][0]["idade"] = "16 anos";
+    $pessoa4[0][0][0][0][0]["cidade"] = "Ivaiporã";
+    $pessoa4[0][0][0][0][0]["característica"] = "inteligente";
+
+    echo ($pessoa1[0][0][0][0][0]["alunos"] . " tem " . $pessoa1[0][0][0][0][0]["idade"] . " anos ");
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
